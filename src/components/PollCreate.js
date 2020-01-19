284d67218a2fb850c1859e7622e19e62337b3647
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-01-19 19:40:24 +0100

    added delete option and enforced 10 options limit

--- src/components/PollCreate.js
+++ src/components/PollCreate.js
@@ -4,7 +4,11 @@ import '../css/create.css'
 class PollCreate extends React.Component {
     constructor(props){
         super(props);
-        this.state = {question: '',currentOption: '', options: []}
+        this.state = {
+            question: '',
+            currentOption: '', 
+            options: [], 
+            maxOptionsReached: false}
         this.handleChange = this.handleChange.bind(this)
     }
 
@@ -18,10 +22,26 @@ class PollCreate extends React.Component {
     }
     handleOptions(e){
         e.preventDefault();
-        let newOptions = this.state.options;
-        newOptions.push(this.state.currentOption)
-        this.props.getOptions(newOptions); //passing "newOptions" to App via props
-        this.setState({options: newOptions, currentOption: ''});
+        if(this.state.options.length <= 9){
+            let newOptions = this.state.options;
+            newOptions.push(this.state.currentOption)
+            this.props.getOptions(newOptions); //passing "newOptions" to App via props
+            this.setState({options: newOptions, currentOption: '', maxOptionsReached: false});
+        }
+        else{
+            this.setState({maxOptionsReached: true});
+        }       
+    }
+    removeOption(e){
+        let arr = this.state.options
+        const removeOptionName = e.target.previousSibling.value;
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] === removeOptionName){
+                arr.splice(i, 1);
+            }
+        }
+        this.props.getOptions(arr); //passing "arr" to App via props
+        this.setState({options: arr, currentOption: ''});
     }
     
 
@@ -52,13 +72,16 @@ class PollCreate extends React.Component {
                     </form>
                     <br/>
                     {this.state.options.map(option => {
-                        return <div key={option}><input type="text" defaultValue={option}></input><button>X</button></div>
+                        return <div key={option}><input type="text" defaultValue={option}></input><button onClick={e => this.removeOption(e)}>X</button></div>
                     })}
                     <br/>
                     <br/>
                     <br/>
                     <div>
                 {this.state.options.length ? <span>{this.state.options.length}/10 possible answers</span> : <span>0/10 possible answers</span> }
+                <br/>
+                {this.state.maxOptionsReached ? <span style={{color : "red"}}>Maximum number of options has been reached </span> : null}
+                <br/>
                     <button>Reset</button>
                     </div>
                 </div>
--- src/components/PollView.js
+++ src/components/PollView.js
@@ -3,12 +3,21 @@ import React from 'react';
 function PollView(props) {
   return (
     <div className="PollView">
-        <h1>Poll View Section</h1>
+      <h1>Poll View Section</h1>
         <br/>
-        <h2>{props.question}</h2>
-        {props.options.map(option => (
-        <p key={option}><input type="radio"></input>{option}</p>
+      {props.question ? <h2>Poll question:{props.question}</h2> : null} 
+       <br/>
+      <ul>
+       {props.options.map(option => (
+        <li key={option} style={{ listStyleType: "none" }}>
+          <input type="radio"></input>{option}
+        </li>
         ))}
+       </ul>
+        <br/>
+        <br/>
+        <br/>
+        <button>Vote</button>
     </div>
   );
 }
